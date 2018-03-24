49699b6a17c076842d2d713ab485866902f87730
maxoon <desfouxmaxime@gmail.com>
2018-03-24 19:06:59 +0100

    add data to users view

--- app/router/routerUsers.js
+++ app/router/routerUsers.js
@@ -1,8 +1,18 @@
 "use strict";
 const express = require('express');
 const routerHome = express.Router();
+const utilisateurBLL_1 = require("../BLL/utilisateurBLL");
 routerHome.get('/', (req, res) => {
-    res.render("users.ejs");
+    let userManager = new utilisateurBLL_1.UtilisateurBLL();
+    let userList, errors;
+    userManager.getAll()
+        .then((users) => {
+        userList = users;
+        res.render("users.ejs", { users: userList, error: null });
+    })
+        .catch((err) => {
+        res.render("users.ejs", { users: null, error: err });
+    });
 });
 module.exports = routerHome;
 //# sourceMappingURL=routerUsers.js.map
--- src/router/routerUsers.ts
+++ src/router/routerUsers.ts
@@ -1,10 +1,24 @@
+import {Utilisateur} from "../BO/utilisateur";
+
 const express = require('express');
 const routerHome = express.Router();
-
+import {UtilisateurBLL} from '../BLL/utilisateurBLL'
 
 
 routerHome.get('/', (req, res)=>{
-    res.render("users.ejs");
+    let userManager = new UtilisateurBLL();
+    let userList, errors;
+    userManager.getAll()
+        .then((users)=>{
+            userList = users;
+            res.render("users.ejs", {users: userList, error: null});
+        })
+        .catch((err)=>{
+            res.render("users.ejs", { users: null, error: err});
+        })
+    ;
+
+
 });
 
 
